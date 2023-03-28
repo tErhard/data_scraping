eadec832788c564d8c41f3dce2acc6c044c5ebf1
telingerErhard <93592012+tErhard@users.noreply.github.com>
2023-03-28 12:05:56 +0300

    Add files via upload

---
+++ moduls/modue_1/tiers_css.py
@@ -0,0 +1,27 @@
+import scrapy
+from hotline.items import HotlineItem
+
+
+class TiersCssSpider(scrapy.Spider):
+    name = "tiers_css"
+    allowed_domains = ["hotline.ua"]
+    start_urls = [
+        f"https://hotline.ua/ua/auto/avtoshiny-i-motoshiny/5012/?p={page}" for page in range(1, 5)]
+
+    def parse(self, response):
+        items = response.css('div.list-body__content').css('.list-item')
+
+        for item in items:
+            name = item.css('a.list-item__title::text').get()
+            brand_name = item.css('a.line-part::text').get()
+            url = item.css('a.list-item__title::attr(href)').get()
+            price = item.css('.price__value::text').get()
+            image_url = item.css('img::attr(src)').get()
+
+            yield HotlineItem(
+                name=name,
+                brand_name=brand_name,
+                price=price,
+                url=url,
+                image_urls=[f"https://hotline.ua{image_url}"]
+            )
---
+++ moduls/modue_1/tiers_xpath.py
@@ -0,0 +1,28 @@
+import scrapy
+from hotline.items import HotlineItem
+
+
+class TiersXpathSpider(scrapy.Spider):
+    name = "tiers_xpath"
+    allowed_domains = ["hotline.ua"]
+    start_urls = [
+        f"https://hotline.ua/ua/auto/avtoshiny-i-motoshiny/5012/?p={page}" for page in range(1, 5)]
+
+    def parse(self, response):
+        items = response.xpath('//div[contains(@class, "list-body__content")]'
+            ).xpath('.//*[contains(@class,"list-item")]')
+
+        for item in items:
+            name = item.xpath('.//a[contains(@class,"list-item__title")]/text()').get()
+            brand_name = item.xpath('.//a[contains(@class,"line-part")]/text()').get()
+            url = item.xpath('.//a[contains(@class,"list-item__title")]/@href').get()
+            price = item.xpath('.//*[@class="price__value"]/text()').get()
+            image_url = item.xpath('.//img/@src').get()
+
+            yield HotlineItem(
+                name=name,
+                brand_name=brand_name,
+                price=price,
+                url=url,
+                image_urls=[f"https://hotline.ua{image_url}"]
+            )
