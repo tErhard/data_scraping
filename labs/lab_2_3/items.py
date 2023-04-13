c9ce00fda20b3fa5b1f01e9f1ddf70579bda5172
telingerErhard <93592012+tErhard@users.noreply.github.com>
2023-04-13 23:30:21 +0300

    Update items.py

--- labs/lab_2_3/items.py
+++ labs/lab_2_3/items.py
@@ -1,1 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
 
+import scrapy
+
+
+class HotlineItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
+    image_urls = scrapy.Field()
+    file_urls = scrapy.Field()
