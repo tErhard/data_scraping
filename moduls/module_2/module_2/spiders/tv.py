298968044c3839fb3e87a5c104af81852a0ae593
telingerErhard <93592012+tErhard@users.noreply.github.com>
2023-05-23 12:37:58 +0300

    Update tv.py

--- moduls/module_2/module_2/spiders/tv.py
+++ moduls/module_2/module_2/spiders/tv.py
@@ -2,36 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = "https://ek.ua/ua/list/160/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
 # Створюємо CSV-файл та вказуємо заголовки стовпців
 csv_file = open("tv_offers.csv", "w", newline="", encoding="utf-8")
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(["Модель", "URL зображення", "Магазин", "Ціна"])
 
-# Отримуємо всі блоки з пропозиціями телевізорів
-tv_blocks = soup.find_all("div", class_="model-short-div")
+# Базовий URL для пропозицій телевізорів
+base_url = "https://ek.ua/ua/list/160/"
 
-# Проходимося по кожному блоку та отримуємо потрібну інформацію
-for block in tv_blocks:
-    model_elem = block.find("a", class_="model-short-title")
-    model = model_elem.text.strip() if model_elem else ""
+# Кількість сторінок для обходу (встановіть відповідну кількість)
+num_pages = 5
 
-    image_elem = block.find("img", class_="model-short-photo")
-    image_url = image_elem["src"] if image_elem else ""
+# Цикл для обходу кожної сторінки з пропозиціями
+for page in range(1, num_pages + 1):
+    # Створення URL-адреси для поточної сторінки
+    url = base_url + "?p=" + str(page)
 
-    shop_elem = block.find("div", class_="model-shop-name")
-    shop = shop_elem.text.strip() if shop_elem else ""
+    # Відправлення запиту на сторінку
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    price_elem = block.find("div", class_="model-price-range")
-    price = price_elem.text.strip() if price_elem else ""
+    # Отримуємо всі блоки з пропозиціями телевізорів
+    tv_blocks = soup.find_all("div", class_="model-short-div")
 
-    # Записуємо дані в CSV-файл
-    csv_writer.writerow([model, image_url, shop, price])
+    # Проходимося по кожному блоку та отримуємо потрібну інформацію
+    for block in tv_blocks:
+        model_elem = block.find("a", class_="model-short-title")
+        model = model_elem.text.strip() if model_elem else ""
+
+        image_elem = block.find("img", class_="model-short-photo")
+        image_url = image_elem["src"] if image_elem else ""
+
+        shop_elem = block.find("div", class_="model-shop-name")
+        shop = shop_elem.text.strip() if shop_elem else ""
+
+        price_elem = block.find("div", class_="model-price-range")
+        price = price_elem.text.strip() if price_elem else ""
+
+        # Записуємо дані в CSV-файл
+        csv_writer.writerow([model, image_url, shop, price])
 
 # Закриваємо CSV-файл
 csv_file.close()
-
-print("Дані успішно збережено у файл tv_offers.csv.")
